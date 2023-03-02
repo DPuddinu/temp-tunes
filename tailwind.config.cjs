9f94b306f1c329a30644d987850ea78de936146a
Dario Puddinu <dario.puddinu92@gmail.com>
2023-03-02 14:36:04 +0100

    added i18n, daisyui

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,9 +2,16 @@
 const config = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        xxs: "425px",
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["dark"],
+  },
 };
 
 module.exports = config;
