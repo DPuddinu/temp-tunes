a53a08c297b44cd865cd220a87887668548f5dbf
Dario Puddinu <dario.puddinu92@gmail.com>
2023-04-27 11:55:34 +0200

    added storybook

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,20 +2,16 @@
 const config = {
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: "./tsconfig.json",
+    project: "./tsconfig.json"
   },
   plugins: ["@typescript-eslint"],
-  extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
+  extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended", "plugin:storybook/recommended"],
   rules: {
-    "@typescript-eslint/consistent-type-imports": [
-      "warn",
-      {
-        prefer: "type-imports",
-        fixStyle: "inline-type-imports",
-      },
-    ],
-    "@typescript-eslint/no-misused-promises": "off",
-  },
+    "@typescript-eslint/consistent-type-imports": ["warn", {
+      prefer: "type-imports",
+      fixStyle: "inline-type-imports"
+    }],
+    "@typescript-eslint/no-misused-promises": "off"
+  }
 };
-
 module.exports = config;
