1f63f39b017ededbd47b593029ac86014b6af734
Dario Puddinu <dario.puddinu@abinsula.com>
2023-05-20 18:55:32 +0200

    added filters to playlist page

--- src/pages/playlist/index.tsx
+++ src/pages/playlist/index.tsx
@@ -1,3 +1,4 @@
+import { Disclosure, Transition } from "@headlessui/react";
 import { ColumnDef, ColumnFiltersState, SortingState, flexRender, getCoreRowModel, getFilteredRowModel, getPaginationRowModel, getSortedRowModel, useReactTable } from "@tanstack/react-table";
 import { GetServerSideProps } from "next";
 import { useTranslation } from "next-i18next";
@@ -20,7 +21,7 @@ const PlaylistsPage: PageWithLayout = () => {
         accessorKey: "name",
       },
       {
-        id:'owner',
+        id:'creator',
         accessorFn: (row) => row.owner.display_name,
       }
     ];
@@ -62,7 +63,72 @@ function DataTable<TData, TValue>({
 
   return (
     <div>
-      <div className=" grid w-full sm:grid-cols-2 md:grid-cols-3 gap-4 overflow-x-auto">
+      <div
+        key="filters"
+        className="mb-4 gap-2 rounded-lg bg-base-200 text-lg font-medium tracking-wide"
+      >
+        <Disclosure>
+          <Disclosure.Button>
+            <div onClick={() => setFilterOpen((open) => !open)} className="p-4">
+              {`Filters ${filterOpen ? "-" : "+"}`}
+            </div>
+          </Disclosure.Button>
+
+          <Transition
+            enter="transition duration-100 ease-out"
+            enterFrom="transform scale-95 opacity-0"
+            enterTo="transform scale-100 opacity-100"
+            leave="transition duration-75 ease-out"
+            leaveFrom="transform scale-100 opacity-100"
+            leaveTo="transform scale-95 opacity-0"
+          >
+            <Disclosure.Panel>
+              <div className="flex flex-wrap gap-2 rounded-b-lg bg-base-200 p-4 md:flex-nowrap">
+                <div className="form-control w-full max-w-xs">
+                  <label className="label">
+                    <span className="label-text">Name</span>
+                  </label>
+                  <input
+                    value={
+                      (table.getColumn("name")?.getFilterValue() as string) ??
+                      ""
+                    }
+                    onChange={(event) =>
+                      table
+                        .getColumn("name")
+                        ?.setFilterValue(event.target.value)
+                    }
+                    type="text"
+                    placeholder=""
+                    className="input-bordered input w-full max-w-xs"
+                  />
+                </div>
+                <div className="form-control w-full max-w-xs">
+                  <label className="label">
+                    <span className="label-text">Creator</span>
+                  </label>
+                  <input
+                    value={
+                      (table
+                        .getColumn("creator")
+                        ?.getFilterValue() as string) ?? ""
+                    }
+                    onChange={(event) => {
+                      table
+                        .getColumn("creator")
+                        ?.setFilterValue(event.target.value);
+                    }}
+                    type="text"
+                    placeholder=""
+                    className="input-bordered input w-full max-w-xs"
+                  />
+                </div>
+              </div>
+            </Disclosure.Panel>
+          </Transition>
+        </Disclosure>
+      </div>
+      <div className=" grid w-full gap-4 overflow-x-auto sm:grid-cols-2 md:grid-cols-3">
         {table.getRowModel().rows?.length ? (
           table
             .getRowModel()
@@ -114,7 +180,7 @@ function PlaylistComponent({name, creator, imageUrl}: PlaylistComponentProps) {
           className="aspect-square h-full w-full rounded-xl"
         ></img>
       </div>
-      <div className="flex flex-col px-2 justify-center gap-2 truncate">
+      <div className="flex flex-col px-4 justify-center gap-2 truncate">
         <p className="font-semibold truncate">{name}</p>
         <p className="text-sm truncate">{creator}</p>
       </div>
