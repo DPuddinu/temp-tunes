8bddbe1db231d179391b9ff03e9a49a2d773e570
Dario Puddinu <dario.puddinu92@gmail.com>
2023-05-31 16:13:08 +0200

    user data context fix

--- src/context/user-data-context.tsx
+++ src/context/user-data-context.tsx
@@ -1,3 +1,4 @@
+import { useSession } from "next-auth/react";
 import { createContext, type ReactNode } from "react";
 import { useStore } from "~/core/store";
 import type { TagsObject } from "~/server/api/routers/prisma_router";
@@ -11,6 +12,7 @@ const initialContext = {
 export const UserDataContext = createContext<Data>(initialContext);
 
 const UserDataProvider = ({ children }: { children: ReactNode }) => {
+  const {data} = useSession()
   const { setTags: setStoreTags, user: storeUser, setUser } = useStore();
 
   // LOADING USER
@@ -19,7 +21,7 @@ const UserDataProvider = ({ children }: { children: ReactNode }) => {
     undefined, 
     { 
       refetchOnWindowFocus: false, 
-      enabled: !storeUser,
+      enabled: !storeUser && data?.user?.id !== undefined,
       onSuccess(data) {
         if (!storeUser && user) {
           setUser(data.user);
