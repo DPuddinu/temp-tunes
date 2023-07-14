cf16de512f348f2fceb06510af01f773ec4cedf6
Dario Puddinu <dario.puddinu@abinsula.com>
2023-07-14 16:33:40 +0200

    removed type RejectPerOperation

--- src/types/prisma-types.ts
+++ src/types/prisma-types.ts
@@ -2,5 +2,5 @@ import type { Prisma, PrismaClient } from "@prisma/client";
 
 export type PrismaClientType = PrismaClient<
   Prisma.PrismaClientOptions,
-  never, | Prisma.RejectPerOperation | undefined
+  never, undefined
 >;
