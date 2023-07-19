07f4b645e74d1fb1b83987140b873fa831046e99
Dario Puddinu <dario.puddinu@abinsula.com>
2023-07-19 17:47:40 +0200

    added create template skeleton

---
+++ src/components/template/CreateTemplateSkeleton.tsx
@@ -0,0 +1,18 @@
+const CreateTemplateSkeleton = () => {
+  return (
+    <div className="flex w-64 animate-pulse flex-col gap-2 rounded-xl bg-base-300 p-2">
+      <div className="h-16 w-full rounded-xl bg-base-200"></div>
+      <div className="h-16 w-full rounded-xl bg-base-200"></div>
+      <div className="h-16 w-full rounded-xl bg-base-200"></div>
+
+      <div className="flex gap-2">
+        <div className="h-16 grow rounded-xl bg-base-200"></div>
+        <div className="h-16 w-16 rounded-full bg-base-200"></div>
+      </div>
+
+      <div className="flex h-16 w-full items-center justify-center rounded-xl bg-base-200"></div>
+    </div>
+  );
+};
+
+export default CreateTemplateSkeleton;
--- src/pages/templates/[id].tsx
+++ src/pages/templates/[id].tsx
@@ -1,42 +1,44 @@
 import { getCookie } from "cookies-next";
 import type { GetServerSideProps } from "next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { useRouter } from "next/router";
 import MainLayout from "~/components/MainLayout";
 import CreateTemplate from "~/components/template/CreateTemplate";
+import CreateTemplateSkeleton from "~/components/template/CreateTemplateSkeleton";
 import TemplateLayout from "~/components/template/TemplatePageLayout";
 import type { Language } from "~/core/settingsStore";
 import { langKey } from "~/hooks/use-language";
 import { useToast } from "~/hooks/use-toast";
 import { type PageWithLayout } from "~/types/page-types";
 import { api } from "~/utils/api";
-import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
 const TemplateById: PageWithLayout = () => {
   const router = useRouter();
   const { setMessage } = useToast();
 
-  const { isLoading, data: templateData } = api.template.getTemplateById.useQuery(
-    { id: router.query.id?.toString() ?? '' },
-    {
-      onSuccess(data){
-        // console.log(data);
-      },
-      onError() {
-        setMessage(`Error: can't get template`);
-      },
-      enabled: router.query.id !== undefined,
-    }
-  );
+  const { isLoading, data: templateData } =
+    api.template.getTemplateById.useQuery(
+      { id: router.query.id?.toString() ?? "" },
+      {
+        onError() {
+          setMessage(`Error: can't get template`);
+        },
+        enabled: router.query.id !== undefined,
+      }
+    );
   return (
     <section className="flex justify-center">
-      <CreateTemplate
-        data={{
-          id: templateData?.id ?? '',
-          name: templateData?.name ?? '',
-          entries: templateData?.templateEntries ?? [],
-          description: templateData?.description ?? ''
-        }}
-      />
+      {isLoading && <CreateTemplateSkeleton />}
+      {templateData && (
+        <CreateTemplate
+          data={{
+            id: templateData?.id ?? "",
+            name: templateData?.name ?? "",
+            entries: templateData?.templateEntries ?? [],
+            description: templateData?.description ?? "",
+          }}
+        />
+      )}
     </section>
   );
 };
