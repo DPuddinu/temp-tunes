a7afcc6ffe72fca95ea2d7510a447fa2c253d51c
Dario Puddinu <dario.puddinu@abinsula.com>
2023-09-06 00:35:50 +0200

    optimization fab group

--- src/components/ui/FabGroup.tsx
+++ src/components/ui/FabGroup.tsx
@@ -1,4 +1,3 @@
-import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
 import { LazyMotion, domAnimation, m } from "framer-motion";
 import { useState, type ReactNode } from "react";
 import { cn } from "~/utils/utils";
@@ -10,47 +9,39 @@ interface props {
   options: ReactNode[];
 }
 const FabGroup = ({ options }: props) => {
-  const [show, setShow] = useState(false);
   const [open, setOpen] = useState(false);
+
   return (
     <LazyMotion features={domAnimation}>
-      <DropdownMenu.Root modal={false} open={open}>
-        <DropdownMenu.Trigger>
-          <Fab
-            className={cn(show && "rotate-45")}
-            onClick={() => {
-              setShow((show) => !show);
-              if (!open) setOpen(true);
-            }}
-          >
-            <PlusSVG />
-          </Fab>
-        </DropdownMenu.Trigger>
-        <DropdownMenu.Content
-          align="end"
-          side="top"
-          className=" translate-y- relative mb-2 mr-2 flex flex-col-reverse items-end gap-4"
-        >
+      <div className="flex flex-col items-end gap-2">
+        <div className="mb-2 mr-2 flex flex-col-reverse items-end gap-4">
           {options?.map((option, i) => (
             <m.div
-              onAnimationComplete={() => {
-                if (!show && i === 0) setOpen(false);
-              }}
               transition={{
-                ease: "easeInOut",
-                delay: show
+                type: "spring",
+                delay: open
                   ? animationDuration * (i + 1)
                   : animationDuration * (options.length - i + 1),
               }}
               initial={{ opacity: 0 }}
-              animate={show ? { opacity: 1 } : { opacity: 0 }}
-              key={i}
+              animate={
+                open
+                  ? { opacity: 1, display: "block" }
+                  : { opacity: 0, transitionEnd: { display: "none" } }
+              }
+              key={"child-" + i}
             >
               {option}
             </m.div>
           ))}
-        </DropdownMenu.Content>
-      </DropdownMenu.Root>
+        </div>
+        <Fab
+          className={cn(open && "rotate-45")}
+          onClick={() => setOpen((open) => !open)}
+        >
+          <PlusSVG />
+        </Fab>
+      </div>
     </LazyMotion>
   );
 };
